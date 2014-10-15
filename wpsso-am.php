c5e91e2de6e3abed2ea5d28098aa378f60beab7a
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-15 13:44:10 +0000

    v1.0.0.1
    git-svn-id: https://plugins.svn.wordpress.org/wpsso-am/trunk@1007911 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsso-am.php
+++ wpsso-am.php
@@ -67,11 +67,11 @@ if ( ! class_exists( 'WpssoAm' ) ) {
 			$shortname = WpssoAmConfig::$cf['plugin']['wpssoam']['short'];
 
 			if ( $this->has_min_ver === false ) {
+				$wpsso_version = $this->p->cf['plugin']['wpsso']['version'];
 				$this->p->debug->log( $shortname.' requires WPSSO version '.$this->min_version.' or newer ('.$wpsso_version.' installed)' );
 				if ( is_admin() )
 					$this->p->notice->err( $shortname.' v'.WpssoAmConfig::$cf['plugin']['wpssoam']['version'].
-					' requires WPSSO v'.$this->min_version.' or newer (version '.
-					$this->p->cf['plugin']['wpsso']['version'].' is currently installed).', true );
+					' requires WPSSO v'.$this->min_version.' or newer ('.$wpsso_version.' is currently installed).', true );
 				return;
 			}
 
