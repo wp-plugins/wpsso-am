a0d845883990170215ad38c330d82ae11d82b3be
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-22 14:34:53 +0000

    v1.0.0.1
    git-svn-id: https://plugins.svn.wordpress.org/wpsso-am/trunk@1012178 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsso-am.php
+++ wpsso-am.php
@@ -6,7 +6,7 @@
  * Author URI: http://surniaulula.com/
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
- * Description: WPSSO extension to provide Apple Store (iTunes) and Google Play App meta tags for Apple's mobile Safari and Twitter's App Card.
+ * Description: WPSSO extension to provide Apple Store / iTunes and Google Play App meta tags for Apple's mobile Safari and Twitter's App Card.
  * Requires At Least: 3.0
  * Tested Up To: 4.0
  * Version: 1.0.0.1
